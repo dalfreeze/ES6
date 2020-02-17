f47a7f34b217ba065b420368026495fb5df2eb57
dalfreeze <bendalferes@gmail.com>
2020-02-17 04:27:14 -0600

    arrow functions continued: this keyword /partial

--- starter/script.js
+++ starter/script.js
@@ -126,3 +126,19 @@ ages6 = years.map((el, i) => {
     const age = now - el;
     return `Age element ${i +1}: ${age}.`
 });
+
+// Arrow functions do not have their own *this* keyword. They have a lexical *this*
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        document.querySelector('.green').addEventListener('click', function(){
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+
+box5.clickMe();
