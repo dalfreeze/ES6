8511cc0e1105aa5aa5d997ef33986b2600ad2955
dalfreeze <bendalferes@gmail.com>
2020-02-22 00:17:13 -0600

    classes and subclasses

--- starter/script.js
+++ starter/script.js
@@ -480,23 +480,94 @@
 // var john5 = new Person5('John', 1990, 'teacher');
 
 
-// ES6
-class Person6 {
-    constructor (name, yearOfBirth, job) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
-    };
+// // ES6
+// class Person6 {
+//     constructor (name, yearOfBirth, job) {
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//     };
 
-    calculateAge() {
-        let age = new Date().getFullYear() - this.yearOfBirth;
-        console.log(age);
-    }
+//     calculateAge() {
+//         let age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     }
 
-    static greetin() { // Not inherited by each instance
-        console.log('Hey there!');
-    }
-};
+//     static greetin() { // Not inherited by each instance
+//         console.log('Hey there!');
+//     }
+// };
 
-const john6 = new Person6('John', 1990, 'teacher');
-john6.calculateAge();
+// const john6 = new Person6('John', 1990, 'teacher');
+// john6.calculateAge();
+
+
+
+
+///////////////////////////////////
+// Lecture: Classes and subclasses
+
+// ES5
+// var Person5 = function(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// };
+
+// Person5.prototype.calculateAge = function() {
+//     let age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+// };
+
+// var Athlete5 = function(name, yearOfBirth, job, olympicsGames, medals) {
+//     Person5.call(this, name, yearOfBirth, job);
+//     this.olympicsGames = olympicsGames;
+//     this.medals = medals;
+// };
+
+// Athlete5.prototype = Object.create(Person5.prototype); // sets Athlete prototype to Person5 prototype
+
+// Athlete5.prototype.wonMedal = function(medal) {
+//     if (medal) {
+//         this.medals = this.medals + medal;
+//     } else {
+//         this.medals++;
+//     };
+//     console.log(this.medals);
+// };
+
+// var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+// johnAthlete5.calculateAge(); // this works because now Athlete inherits anything Person5 has
+// johnAthlete5.wonMedal(5);
+
+
+// // ES6
+// class Person6 {
+//     constructor (name, yearOfBirth, job) {
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//     };
+
+//     calculateAge() {
+//         let age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     };
+// };
+
+// class Athlete6 extends Person6 { // how to create subclass
+//     constructor(name, yearOfBirth, job, olympicGames, medals) {
+//         super(name, yearOfBirth, job);
+//         this.olympicGames = olympicGames;
+//         this.medals = medals;
+//     }
+
+//     wonMedal() {
+//         this.medals++;
+//         console.log(this.medals);
+//     }
+// }
+
+// const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+// johnAthlete6.calculateAge();
+// johnAthlete6.wonMedal();
